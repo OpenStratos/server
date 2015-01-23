3b3c6ca72f839084c21d13dc8c75ac25b402361b
Razican <admin@razican.com>
2015-01-23 11:19:00 +0100

    Fixed build script

--- testing/.travis-ci.sh
+++ testing/.travis-ci.sh
@@ -54,7 +54,7 @@ function setup_arm_chroot {
     sudo touch ${CHROOT_DIR}/.chroot_is_done
 
     # Call ourselves again which will cause tests to run
-    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./.travis-ci.sh"
+    sudo chroot ${CHROOT_DIR} bash -c "cd ${TRAVIS_BUILD_DIR} && ./testing/.travis-ci.sh"
 }
 
 if [ -e "/.chroot_is_done" ]; then
