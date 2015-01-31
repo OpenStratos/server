50227606deea805bca97755921e032f8bf549ce4
Razican <admin@razican.com>
2015-01-31 19:52:34 +0100

    Better build script

---
+++ testing/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -e "/.chroot_is_done" ]; then
+	export LANGUAGE="en_US.UTF-8"
+
+	printf "\n\n"
+	printf "|------------------------------------------------------------|\n\n"
+	printf "Environment: $(uname -a)"
+	printf "|------------------------------------------------------------|\n\n"
+
+	# Installing WiringPi
+	printf "Installing WiringPi\n"
+	git clone https://github.com/OpenStratos/WiringPi.git > /dev/null
+	cd WiringPi
+	./build > /dev/null
+	cd ..
+
+	# Start build
+	./build.sh
+else
+	touch /tmp/arm-chroot/${TRAVIS_BUILD_DIR}/.chroot_is_done
+	sudo chroot /tmp/arm-chroot bash -c "cd ${TRAVIS_BUILD_DIR} && ./testing/test.sh"
+fi
