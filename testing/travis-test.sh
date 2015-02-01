b5f399d0377414f0455ceda812d13abb818c588b
Razican <admin@razican.com>
2015-02-01 12:12:12 +0100

    Fixed build script
    Let's hope this is the last time.

--- testing/travis-test.sh
+++ testing/travis-test.sh
@@ -18,5 +18,5 @@ if [ -e "/.chroot_is_done" ]; then
 	./build.sh
 else
 	touch /tmp/arm-chroot${TRAVIS_BUILD_DIR}/.chroot_is_done
-	sudo chroot /tmp/arm-chroot bash -c "cd ${TRAVIS_BUILD_DIR} && ./testing/test.sh"
+	sudo chroot /tmp/arm-chroot bash -c "cd ${TRAVIS_BUILD_DIR} && ./testing/travis-test.sh"
 fi
