44f012b7cd297d4e523843871fe7546145f33e70
Razican <admin@razican.com>
2015-02-25 15:15:16 +0100

    Added camera test

--- testing/camera_test.cc
+++ testing/camera_test.cc
@@ -16,4 +16,18 @@ describe("Camera", [](){
 		Camera::get_instance().stop();
 		AssertThat(Camera::get_instance().is_recording(), Equals(false));
 	});
+
+	#ifdef RASPIVID
+		it("file creation test", [&](){
+			Camera::get_instance().record(10);
+			this_thread::sleep_for(chrono::seconds(11));
+
+			struct stat buf;
+			int result = stat("os_video.h264", &buf);
+
+			AssertThat(result, Equals(0));
+		});
+	#else
+		it_skip("file creation test", [&](){});
+	#endif
 });
