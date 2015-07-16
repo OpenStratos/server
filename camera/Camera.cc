3f0f05d08a84fddc5e17d4e42e2d491c53a79af2
Razican <admin@razican.com>
2015-07-16 14:32:27 +0200

    Removed debug information

--- camera/Camera.cc
+++ camera/Camera.cc
@@ -7,8 +7,6 @@
 #include <thread>
 #include <chrono>
 
-#include <iostream>
-
 #include <sys/types.h>
 #include <dirent.h>
 
@@ -34,30 +32,21 @@ void Camera::record_thread(int time)
 
 void Camera::record(int time)
 {
-	cout << "record called" << endl;
-	cout << "is recording?: " << this->recording << endl;
 	if ( ! this->recording)
 	{
-		cout << "Not recording, so creating command" << endl;
 		string command;
 		if (time > 0)
 		{
-			cout << "Command with time." << endl;
 			command = "raspivid -o data/video/test.h264 -t " + to_string(time) + " &";
 		}
 		else
 		{
-			cout << "Command without time." << endl;
 			command = "raspivid -o data/video/video-"+ to_string(get_file_count("data/video/"))
 				+".h264 -t " + to_string(time) + " &";
-			cout << "Command created." << endl;
-			cout << "Command: '" << command << "'" << endl;
 		}
 		#ifndef RASPIVID
 			command = "";
 		#endif
-
-		cout << "Command: '" << command << "'" << endl;
 
 		system(command.c_str());
 		this->recording = true;
