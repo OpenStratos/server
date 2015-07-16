ec234b3fdf2e14617cae3600ef816ba4a38edac1
Razican <admin@razican.com>
2015-07-16 14:28:24 +0200

    Created data directories in build script

--- build.sh
+++ build.sh
@@ -7,5 +7,12 @@ autoconf
 ./configure
 make utesting
 
+mkdir data
+mkdir data/logs
+mkdir data/logs/GPS
+mkdir data/logs/camera
+mkdir data/logs/main
+mkdir data/video
+
 printf "\n\n----- Starting unit tests -----\n\n"
 ./utesting
