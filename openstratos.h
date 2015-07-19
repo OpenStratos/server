9bad25fa8a0a85366ffb3b9b1cf2076aa84c491d
Razican <admin@razican.com>
2015-07-19 21:16:16 +0200

    Small fixes

--- openstratos.h
+++ openstratos.h
@@ -2,6 +2,7 @@
 #define OPENSTRATOS_H_
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <thread>
 #include <functional>
@@ -11,6 +12,7 @@
 #include <sys/time.h>
 
 #include "config.h"
+#include "constants.h"
 #include "logger/Logger.h"
 #include "gps/GPS.h"
 #include "camera/Camera.h"
@@ -30,6 +32,8 @@ namespace os
 	inline bool file_exists(const string& name);
 	inline float get_available_disk_space();
 	void gps_thread_fn(State& state);
+	State set_state(State new_state);
+	string state_to_string(State state);
 }
 
 using namespace std;
