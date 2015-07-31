aa8496881a8d19604d74c41ced816cff738db737
Razican <admin@razican.com>
2015-07-31 12:09:02 +0200

    More progress in the server script. Still some SMS failing.

--- gsm/GSM.cc
+++ gsm/GSM.cc
@@ -5,6 +5,8 @@
 #include <string>
 #include <sstream>
 
+#include <sys/time.h>
+
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
@@ -19,32 +21,60 @@ GSM& GSM::get_instance()
 
 GSM::~GSM()
 {
-	this->serial.close();
+	if (this->serial.is_open())
+	{
+		this->logger->log("Closing serial interface...");
+		this->serial.close();
+		this->logger->log("Serial interface closed.");
+
+		this->logger->log("Deallocating command logger...");
+		delete this->command_logger;
+	}
+	this->logger->log("Shutting down...");
+	this->turn_off();
+	this->logger->log("Shut down finished.");
+	delete this->logger;
 }
 
 bool GSM::initialize(int pwr_gpio, int status_gpio, const string& serial_URL)
 {
+	struct timeval timer;
+	gettimeofday(&timer, NULL);
+	struct tm * now = gmtime(&timer.tv_sec);
+
+	this->logger = new Logger("data/logs/GSM/GSM."+ to_string(now->tm_year+1900) +"-"+ to_string(now->tm_mon) +"-"+
+		to_string(now->tm_mday) +"."+ to_string(now->tm_hour) +"-"+ to_string(now->tm_min) +"-"+
+		to_string(now->tm_sec) +".log", "GSM");
+
 	this->pwr_gpio = pwr_gpio;
 	this->status_gpio = status_gpio;
 
-	if ( ! this->serial.initialize(serial_URL, 9600))
+	this->logger->log("Starting serial connection...");
+	if ( ! this->serial.initialize(serial_URL, GSM_BAUDRATE))
 	{
+		this->logger->log("GSM serial error.");
 		return false;
 	}
+	this->logger->log("Serial connection started.");
+
+	this->command_logger = new Logger("data/logs/GSM/GSMCommands."+ to_string(now->tm_year+1900) +"-"+
+		to_string(now->tm_mon) +"-"+ to_string(now->tm_mday) +"."+ to_string(now->tm_hour) +"-"+
+		to_string(now->tm_min) +"-"+ to_string(now->tm_sec) +".log", "GSMCommand");
 
 	pinMode(this->pwr_gpio, OUTPUT);
 	digitalWrite(this->pwr_gpio, HIGH);
-	pinMode(this->pwr_gpio, INPUT);
-
-	this_thread::sleep_for(3s);
-
+	pinMode(this->status_gpio, INPUT);
 	return true;
 }
 
 bool GSM::send_SMS(const string& message, const string& number) const
 {
+	this->logger->log("Sending SMS: \""+message+"\" to number "+number+".");
+
 	if (this->send_command_read("AT+CMGF=1") != "OK")
 	{
+		this->logger->log("Error sending SMS");
+		this->logger->log("Primer condicional");
 		return false;
 	}
 
@@ -52,14 +82,17 @@ bool GSM::send_SMS(const string& message, const string& number) const
 
 	send_command << "AT+CMGS=\"" << number << "\"";
 
-	if (this->send_command_read(send_command.str()) != "> ")
+	if ( ! this->send_command_read_only(send_command.str(), ">"))
 	{
+		this->logger->log("Error sending SMS.");
+		this->logger->log("Segundo condicional");
 		return false;
 	}
 
-	this->serial.send(message+"\x1a"+GSM_ENDL);
+	this->send_command(message+"\x1a\r\n");
 	this->serial.flush();
 
+	this->logger->log("SMS sent.");
 	return true;
 }
 
@@ -87,7 +120,13 @@ bool GSM::get_status() const
 
 bool GSM::is_up() const
 {
-	return this->send_command_read("AT") == "OK";
+	return this->send_command_read_only("AT", "OK");
+}
+
+bool GSM::has_connectivity() const
+{
+	   string response = this->send_command_read("AT+CREG?");
+	   return response == "+CREG: 0,1" || response == "+CREG: 0,5";
 }
 
 bool GSM::turn_on() const
@@ -143,9 +182,38 @@ bool GSM::tear_down_GPRS() const
 
 const string GSM::send_command_read(const string& command) const
 {
+	this->command_logger->log("Sent: '"+command+"'");
 	this->serial.flush();
 	this->serial.send(command);
+	// Sent command
+	this->serial.read_line();
 	string response = this->serial.read_line();
+	this->command_logger->log("Received: '"+response+"'");
 	this->serial.flush();
 	return response;
+}
+
+bool GSM::send_command_read_only(const string& command, const string& only) const
+{
+	this->command_logger->log("Sent: '"+command+"'");
+	this->serial.flush();
+	this->serial.send(command);
+	// Sent command
+	this->serial.read_line();
+	bool response = this->serial.read_only(only);
+	if (response)
+	{
+		this->command_logger->log("Received: '"+only+"'");
+	}
+	this->serial.flush();
+	return response;
+}
+
+void GSM::send_command(const string& command) const
+{
+	this->command_logger->log("Sent: '"+command+"'");
+	this->serial.flush();
+	this->serial.send(command);
+	this_thread::sleep_for(25ms);
+	this->serial.flush();
 }
