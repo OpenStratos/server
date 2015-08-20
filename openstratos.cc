51bc8cb7a20fe264d8a0e21b90e9f596ce28dd6a
Razican <admin@razican.com>
2015-08-20 16:42:51 +0200

    Better logger output on batteries

--- openstratos.cc
+++ openstratos.cc
@@ -133,8 +133,8 @@ int main(void)
 	logger.log("Checking batteries...");
 	double main_battery, gsm_battery;
 	GSM::get_instance().get_battery_status(main_battery, gsm_battery);
-	logger.log("Batteries checked => Main battery: "+ to_string(main_battery*100) +
-		"% - GSM battery: "+ to_string(gsm_battery*100) +"%");
+	logger.log("Batteries checked => Main battery: "+ (main_battery > -1 ? to_string(main_battery*100)+"%" : "disconnected") +
+		" - GSM battery: "+ to_string(gsm_battery*100) +"%");
 
 	if ((main_battery < 0.95  && main_battery > -1) || gsm_battery < 0.95)
 	{
