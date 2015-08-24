0d9240415595d12eab25fbfae0b7efa097171c47
Razican <admin@razican.com>
2015-08-24 16:10:39 +0200

    Fixed build

--- gsm/GSM.h
+++ gsm/GSM.h
@@ -2,6 +2,7 @@
 #define GSM_GSM_H_
 
 #include <string>
+#include <atomic>
 
 #include "serial/Serial.h"
 #include "logger/Logger.h"
@@ -23,8 +24,6 @@ namespace os {
 		GSM() = default;
 
 		const string send_command_read(const string& command) const;
-		bool send_command_read_only(const string& command, const string& only) const;
-		void send_command(const string& command) const;
 		bool init_GPRS() const;
 		bool tear_down_GPRS() const;
 	public:
@@ -33,12 +32,11 @@ namespace os {
 		static GSM& get_instance();
 
 		bool initialize();
-		bool send_SMS(const string& message, const string& number) const;
-		bool get_location(double& latitude, double& longitude) const;
+		bool send_SMS(const string& message, const string& number);
+		bool get_location(double& latitude, double& longitude);
 		bool get_status() const;
-		bool get_battery_status(double& main_bat_percentage, double& gsm_bat_percentage) const;
-		bool is_up() const;
-		bool has_connectivity() const;
+		bool get_battery_status(double& main_bat_percentage, double& gsm_bat_percentage);
+		bool has_connectivity();
 		bool turn_on() const;
 		bool turn_off() const;
 	};
