df6a80f779aa5eca69f78fb238ab362296eb3c32
Razican <admin@razican.com>
2015-09-19 21:53:22 +0200

    Fixed SMSs

--- openstratos.cc
+++ openstratos.cc
@@ -967,7 +967,7 @@ void os::go_down(Logger* logger)
 			(bat_status ? "Main bat: "+ to_string((int) (main_battery*100)) +"%\r\n"+
 				"GSM bat: "+ to_string((int) (gsm_battery*100)) +"%\r\n" : "Bat: ERR\r\n") +
 			"Fix: "+ (GPS::get_instance().is_fixed() ? "OK" : "ERR") +
-			"Sat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
+			"\r\nSat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
 		{
 			logger->log("Error sending first SMS.");
 		}
@@ -1029,7 +1029,7 @@ void os::go_down(Logger* logger)
 				(bat_status ? "Main bat: "+ to_string((int) (main_battery*100)) +"%\r\n"+
 					"GSM bat: "+ to_string((int) (gsm_battery*100)) +"%\r\n" : "Bat: ERR\r\n") +
 				"Fix: "+ (GPS::get_instance().is_fixed() ? "OK" : "ERR") +
-				"Sat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
+				"\r\nSat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
 			{
 				logger->log("Error sending second SMS.");
 			}
@@ -1091,7 +1091,7 @@ void os::go_down(Logger* logger)
 				(bat_status ? "Main bat: "+ to_string((int) (main_battery*100)) +"%\r\n"+
 					"GSM bat: "+ to_string((int) (gsm_battery*100)) +"%\r\n" : "Bat: ERR\r\n") +
 				"Fix: "+ (GPS::get_instance().is_fixed() ? "OK" : "ERR") +
-				"Sat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
+				"\r\nSat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
 			{
 				logger->log("Error sending third SMS.");
 			}
@@ -1141,7 +1141,7 @@ void os::land(Logger* logger)
 		(bat_status ? "Main bat: "+ to_string((int) (main_battery*100)) +"%\r\n"+
 			"GSM bat: "+ to_string((int) (gsm_battery*100)) +"%\r\n" : "Bat: ERR\r\n") +
 		"Fix: "+ (GPS::get_instance().is_fixed() ? "OK" : "ERR") +
-		"Sat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
+		"\r\nSat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE))
 	{
 		logger->log("Error sending landed SMS. Trying again in 10 minutes...");
 	}
@@ -1160,7 +1160,7 @@ void os::land(Logger* logger)
 		(bat_status ? "Main bat: "+ to_string((int) (main_battery*100)) +"%\r\n"+
 			"GSM bat: "+ to_string((int) (gsm_battery*100)) +"%\r\n" : "Bat: ERR\r\n") +
 		"Fix: "+ (GPS::get_instance().is_fixed() ? "OK" : "ERR") +
-		"Sat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE) ||
+		"\r\nSat: "+ to_string(GPS::get_instance().get_satellites()), SMS_PHONE) ||
 		! GPS::get_instance().is_fixed()) &&
 		(main_battery >= 0 || main_battery < -1) && gsm_battery >= 0)
 	{
