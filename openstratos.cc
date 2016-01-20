842f1f461b95e71e9df52d7a968eac5f6ee8a929
Razican <razican@protonmail.ch>
2016-01-20 20:13:30 +0100

    Added GSM and GPS power off if camera test fails

--- openstratos.cc
+++ openstratos.cc
@@ -487,6 +487,18 @@ void os::initialize(Logger* logger, tm* now)
 	{
 		logger->log("Error starting recording");
 
+		logger->log("Turning GSM off...");
+		if (GSM::get_instance().turn_off())
+			logger->log("GSM off.");
+		else
+			logger->log("Error turning GSM off.");
+
+		logger->log("Turning GPS off...");
+		if (GPS::get_instance().turn_off())
+			logger->log("GPS off.");
+		else
+			logger->log("Error turning GPS off.");
+
 		#ifndef NO_POWER_OFF
 			sync();
 			reboot(RB_POWER_OFF);
