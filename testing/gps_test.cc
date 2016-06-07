96dc508305d395d39c771cd7aa3415b3b63c95af
Razican <razican@protonmail.ch>
2016-06-07 11:40:34 +0200

    Fixed some small issues

--- testing/gps_test.cc
+++ testing/gps_test.cc
@@ -21,21 +21,21 @@ describe("GPS", [](){
 	});
 
 	it("GGA frame parser test", [&](){
-		GPS::get_instance().parse("$GPGGA,151025,2011.3454,N,12020.2464,W,1,05,1.53,20134.13,M,20103.45,M,,*56");
+		GPS::get_instance().parse("$GNGGA,065022.90,4518.48425,N,00113.59390,W,1,05,2.33,10202.2,M,10235.9,M,,*60");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(true));
 
 		tm gps_time = GPS::get_instance().get_time();
-		AssertThat(gps_time.tm_hour, Equals(15));
-		AssertThat(gps_time.tm_min, Equals(10));
-		AssertThat(gps_time.tm_sec, Equals(25));
+		AssertThat(gps_time.tm_hour, Equals(6));
+		AssertThat(gps_time.tm_min, Equals(50));
+		AssertThat(gps_time.tm_sec, Equals(22));
 
 		AssertThat(GPS::get_instance().get_satellites(), Equals(5));
-		AssertThat(GPS::get_instance().get_latitude(), Is().EqualToWithDelta(20.18909, 0.00001));
-		AssertThat(GPS::get_instance().get_longitude(), Is().EqualToWithDelta(-120.33744, 0.00001));
+		AssertThat(GPS::get_instance().get_latitude(), Is().EqualToWithDelta(45.308070833, 0.00001));
+		AssertThat(GPS::get_instance().get_longitude(), Is().EqualToWithDelta(-1.226565, 0.00001));
 
-		AssertThat(GPS::get_instance().get_HDOP(), Is().EqualToWithDelta(1.53, 0.0005));
-		AssertThat(GPS::get_instance().get_altitude(), Is().EqualToWithDelta(20134.13, 0.0005));
+		AssertThat(GPS::get_instance().get_HDOP(), Is().EqualToWithDelta(2.33, 0.0005));
+		AssertThat(GPS::get_instance().get_altitude(), Is().EqualToWithDelta(10202.2, 0.0005));
 	});
 
 	it("GGA frame parser pass test", [&](){
@@ -46,13 +46,13 @@ describe("GPS", [](){
 		double altitude = GPS::get_instance().get_altitude();
 		float hdop = GPS::get_instance().get_HDOP();
 
-		GPS::get_instance().parse("$GPGGA,170810,2316.3654,S,12225.2464,E,0,07,1.89,18647.15,M,18640.35,M,,*53");
+		GPS::get_instance().parse("$GNGGA,054320.70,4416.46425,N,00512.59490,W,0,05,1.25,9502.63,M,9515.2,M,,*55");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(false));
 
-		AssertThat(GPS::get_instance().get_time().tm_hour, Equals(17));
-		AssertThat(GPS::get_instance().get_time().tm_min, Equals(8));
-		AssertThat(GPS::get_instance().get_time().tm_sec, Equals(10));
+		AssertThat(GPS::get_instance().get_time().tm_hour, Equals(5));
+		AssertThat(GPS::get_instance().get_time().tm_min, Equals(43));
+		AssertThat(GPS::get_instance().get_time().tm_sec, Equals(20));
 
 		AssertThat(GPS::get_instance().get_satellites(), Equals(satellites));
 		AssertThat(GPS::get_instance().get_latitude(), Equals(latitude));
@@ -62,43 +62,50 @@ describe("GPS", [](){
 	});
 
 	it("GSA frame parser test", [&](){
-		GPS::get_instance().parse("$GPGSA,A,3,,,,,,16,18,,22,24,,,3.6,2.1,2.2*3C");
+		GPS::get_instance().parse("$GNGSA,A,3,22,01,19,23,17,03,11,31,,,,,1.91,0.99,1.63*1E");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(true));
-		AssertThat(GPS::get_instance().get_HDOP(), Is().EqualToWithDelta(2.1, 0.0005));
-		AssertThat(GPS::get_instance().get_VDOP(), Is().EqualToWithDelta(2.2, 0.0005));
+		AssertThat(GPS::get_instance().get_PDOP(), Is().EqualToWithDelta(1.91, 0.0005));
+		AssertThat(GPS::get_instance().get_HDOP(), Is().EqualToWithDelta(0.99, 0.0005));
+		AssertThat(GPS::get_instance().get_VDOP(), Is().EqualToWithDelta(1.63, 0.0005));
 	});
 
 	it("GSA frame parser pass test", [&](){
+		float pdop = GPS::get_instance().get_PDOP();
 		float hdop = GPS::get_instance().get_HDOP();
 		float vdop = GPS::get_instance().get_VDOP();
 
-		GPS::get_instance().parse("$GPGSA,A,1,19,28,14,18,27,22,31,39,,,,,1.7,1.0,1.3*36");
+		GPS::get_instance().parse("$GNGSA,A,1,22,01,19,23,17,03,11,31,,,,,2.30,0.05,1.52*13");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(false));
+		AssertThat(GPS::get_instance().get_PDOP(), Equals(pdop));
 		AssertThat(GPS::get_instance().get_HDOP(), Equals(hdop));
 		AssertThat(GPS::get_instance().get_VDOP(), Equals(vdop));
 	});
 
 	it("RMC frame parser test", [&](){
-		GPS::get_instance().parse("$GPRMC,225446,A,4916.45,N,12311.12,W,000.5,054.7,191194,020.3,E*68");
+		GPS::get_instance().parse("$GNRMC,083250.50,A,4210.48220,N,00306.55496,W,0.072,0.25,070616,,,A*6A");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(true));
 
 		tm gps_time = GPS::get_instance().get_time();
 
-		AssertThat(gps_time.tm_mday, Equals(19));
-		AssertThat(gps_time.tm_mon, Equals(10));
-		AssertThat(gps_time.tm_year, Equals(194));
-		AssertThat(gps_time.tm_hour, Equals(22));
-		AssertThat(gps_time.tm_min, Equals(54));
-		AssertThat(gps_time.tm_sec, Equals(46));
+		AssertThat(gps_time.tm_mday, Equals(7));
+		AssertThat(gps_time.tm_mon, Equals(5));
+		AssertThat(gps_time.tm_year, Equals(116));
+		AssertThat(gps_time.tm_hour, Equals(8));
+		AssertThat(gps_time.tm_min, Equals(32));
+		AssertThat(gps_time.tm_sec, Equals(50));
 
-		AssertThat(GPS::get_instance().get_latitude(), Is().EqualToWithDelta(49.27417, 0.00001));
-		AssertThat(GPS::get_instance().get_longitude(), Is().EqualToWithDelta(-123.18533, 0.00001));
+		AssertThat(GPS::get_instance().get_latitude(),
+					Is().EqualToWithDelta(42.174703333, 0.00001));
+		AssertThat(GPS::get_instance().get_longitude(),
+					Is().EqualToWithDelta(-3.109249333, 0.00001));
 
-		AssertThat(GPS::get_instance().get_velocity().speed, Is().EqualToWithDelta(0.25722, 0.00001));
-		AssertThat(GPS::get_instance().get_velocity().course, Is().EqualToWithDelta(54.7, 0.001));
+		AssertThat(GPS::get_instance().get_velocity().speed,
+					Is().EqualToWithDelta(0.03704, 0.00001));
+		AssertThat(GPS::get_instance().get_velocity().course,
+					Is().EqualToWithDelta(0.25, 0.001));
 	});
 
 	it("RMC frame parser pass test", [&](){
@@ -108,18 +115,18 @@ describe("GPS", [](){
 		float speed = GPS::get_instance().get_velocity().speed;
 		float course = GPS::get_instance().get_velocity().course;
 
-		GPS::get_instance().parse("$GPRMC,081836,V,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*75");
+		GPS::get_instance().parse("$GNRMC,093024.50,V,4012.20220,N,00508.50496,W,0.015,0.80,060515,,,A*71");
 
 		AssertThat(GPS::get_instance().is_fixed(), Equals(false));
 
 		tm new_gps_time = GPS::get_instance().get_time();
 
-		AssertThat(new_gps_time.tm_hour, Equals(8));
-		AssertThat(new_gps_time.tm_min, Equals(18));
-		AssertThat(new_gps_time.tm_sec, Equals(36));
-		AssertThat(new_gps_time.tm_mday, Equals(13));
-		AssertThat(new_gps_time.tm_mon, Equals(8));
-		AssertThat(new_gps_time.tm_year, Equals(198));
+		AssertThat(new_gps_time.tm_hour, Equals(9));
+		AssertThat(new_gps_time.tm_min, Equals(30));
+		AssertThat(new_gps_time.tm_sec, Equals(24));
+		AssertThat(new_gps_time.tm_mday, Equals(6));
+		AssertThat(new_gps_time.tm_mon, Equals(4));
+		AssertThat(new_gps_time.tm_year, Equals(115));
 
 		AssertThat(GPS::get_instance().get_latitude(), Equals(latitude));
 		AssertThat(GPS::get_instance().get_longitude(), Equals(longitude));
